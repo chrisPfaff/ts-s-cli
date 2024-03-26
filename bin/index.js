3587f02a828079550dd3cf28d55bdcb2830a6cf8
chrisPfaff <chrispfaff10@gmail.com>
2024-03-26 17:41:48 -0500

    Added axios and yargs options

--- bin/index.js
+++ bin/index.js
@@ -15,37 +15,70 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const node_process_1 = __importDefault(require("node:process"));
-const yargs_1 = __importDefault(require("yargs/yargs"));
+const yargs_1 = __importDefault(require("yargs"));
+yargs_1.default
+    .options({
+    name: {
+        alias: "n",
+        describe: "Name of the artist",
+        type: "string",
+    },
+})
+    .parse();
 const argv = (0, yargs_1.default)(node_process_1.default.argv).argv;
-(() => {
-    function getToken() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield fetch("https://accounts.spotify.com/api/token", {
-                method: "POST",
-                body: new URLSearchParams({
-                    grant_type: "client_credentials",
-                }),
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    Authorization: "Basic " +
-                        Buffer.from(node_process_1.default.env.API + ":" + node_process_1.default.env.SECRET).toString("base64"),
-                },
-            });
-            return yield response.json();
-        });
-    }
-    function getArtistInfo(access_token, name) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield fetch(`https://api.spotify.com/v1/search?q=${name}&type=artist&limit=1`, {
-                method: "GET",
-                headers: { Authorization: "Bearer " + access_token },
-            });
-            return yield response.json();
-        });
-    }
-    getToken().then((response) => {
-        getArtistInfo(response.access_token, argv.name).then((profile) => {
-            console.log(profile.artists.items);
-        });
+const access_token = [];
+if (!argv.name) {
+    console.error("Please provide a name");
+    node_process_1.default.exit(1);
+}
+const getToken = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        body: new URLSearchParams({
+            grant_type: "client_credentials",
+        }),
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: "Basic " +
+                Buffer.from(node_process_1.default.env.API + ":" + node_process_1.default.env.SECRET).toString("base64"),
+        },
     });
-})();
+    const json = yield response.json();
+    access_token.push(json.access_token);
+});
+getToken();
+console.log(access_token);
+// (() => {
+//   async function getToken() {
+//     const response = await fetch("https://accounts.spotify.com/api/token", {
+//       method: "POST",
+//       body: new URLSearchParams({
+//         grant_type: "client_credentials",
+//       }),
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded",
+//         Authorization:
+//           "Basic " +
+//           Buffer.from(process.env.API + ":" + process.env.SECRET).toString(
+//             "base64"
+//           ),
+//       },
+//     });
+//     return await response.json();
+//   }
+//   async function getArtistInfo(access_token: string, name: string) {
+//     const response = await fetch(
+//       `https://api.spotify.com/v1/search?q=${name}&type=artist&limit=1`,
+//       {
+//         method: "GET",
+//         headers: { Authorization: "Bearer " + access_token },
+//       }
+//     );
+//     return await response.json();
+//   }
+//   getToken().then((response) => {
+//     getArtistInfo(response.access_token, argv.name).then((profile) => {
+//       console.log(profile.artists.items);
+//     });
+//   });
+// })();
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@
 
 import "dotenv/config";
 
+import axios from "axios";
 import process from "node:process";
-import yargs from "yargs/yargs";
+import yargs from "yargs";
 
 interface Arguments {
   [x: string]: unknown;
@@ -12,43 +13,81 @@ interface Arguments {
   name: string;
   api: string;
 }
+
+yargs
+  .options({
+    name: {
+      alias: "n",
+      describe: "Name of the artist",
+      type: "string",
+    },
+  })
+  .parse();
 const argv = yargs(process.argv).argv as Arguments;
 
-(() => {
-  async function getToken() {
-    const response = await fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
-      body: new URLSearchParams({
-        grant_type: "client_credentials",
-      }),
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization:
-          "Basic " +
-          Buffer.from(process.env.API + ":" + process.env.SECRET).toString(
-            "base64"
-          ),
-      },
-    });
+const access_token: string[] = [];
+if (!argv.name) {
+  console.error("Please provide a name");
+  process.exit(1);
+}
 
-    return await response.json();
-  }
-
-  async function getArtistInfo(access_token: string, name: string) {
-    const response = await fetch(
-      `https://api.spotify.com/v1/search?q=${name}&type=artist&limit=1`,
-      {
-        method: "GET",
-        headers: { Authorization: "Bearer " + access_token },
-      }
-    );
-
-    return await response.json();
-  }
-
-  getToken().then((response) => {
-    getArtistInfo(response.access_token, argv.name).then((profile) => {
-      console.log(profile.artists.items);
-    });
+const getToken = async () => {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+    }),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        Buffer.from(process.env.API + ":" + process.env.SECRET).toString(
+          "base64"
+        ),
+    },
   });
-})();
+
+  const json = await response.json();
+  access_token.push(json.access_token);
+};
+
+getToken();
+console.log(access_token);
+// (() => {
+//   async function getToken() {
+//     const response = await fetch("https://accounts.spotify.com/api/token", {
+//       method: "POST",
+//       body: new URLSearchParams({
+//         grant_type: "client_credentials",
+//       }),
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded",
+//         Authorization:
+//           "Basic " +
+//           Buffer.from(process.env.API + ":" + process.env.SECRET).toString(
+//             "base64"
+//           ),
+//       },
+//     });
+
+//     return await response.json();
+//   }
+
+//   async function getArtistInfo(access_token: string, name: string) {
+//     const response = await fetch(
+//       `https://api.spotify.com/v1/search?q=${name}&type=artist&limit=1`,
+//       {
+//         method: "GET",
+//         headers: { Authorization: "Bearer " + access_token },
+//       }
+//     );
+
+//     return await response.json();
+//   }
+
+//   getToken().then((response) => {
+//     getArtistInfo(response.access_token, argv.name).then((profile) => {
+//       console.log(profile.artists.items);
+//     });
+//   });
+// })();
