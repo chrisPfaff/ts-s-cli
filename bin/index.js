a95ca2df7e3573848c5409b985718ec103a2b6dd
chrisPfaff <chrispfaff10@gmail.com>
2024-03-29 11:12:58 -0500

    removed yargs and axios for commander actions

--- bin/index.js
+++ bin/index.js
@@ -14,70 +14,62 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
-const yargs_1 = __importDefault(require("yargs"));
 const fs_1 = __importDefault(require("fs"));
 const node_process_1 = __importDefault(require("node:process"));
-yargs_1.default
-    .usage("Usage: nodejs-spotify-cli <ts-s-cli> [options]")
-    .options({
-    n: {
-        describe: "Search with name of the artist",
-        type: "string",
-    },
-    a: {
-        describe: "Add api key and secret in [api secret] format ignore brackets",
-        type: "array",
-    },
-})
-    .parse();
-const argv = (0, yargs_1.default)(node_process_1.default.argv).argv;
-const argsArray = yargs_1.default.array("a").argv;
-const getToken = () => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        body: new URLSearchParams({
-            grant_type: "client_credentials",
-        }),
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: "Basic " +
-                Buffer.from(node_process_1.default.env.API + ":" + node_process_1.default.env.SECRET).toString("base64"),
-        },
+const { Command } = require("commander");
+const program = new Command();
+program
+    .name("string-util")
+    .description("CLI to some JavaScript string utilities")
+    .version("0.8.0");
+program
+    .command("keys")
+    .description("Input client ID and Secret from https://developer.spotify.com/dashboard/applications")
+    .arguments("<ClientID> <Secret>")
+    .action((clientID, secret) => {
+    const getToken = () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            body: new URLSearchParams({
+                grant_type: "client_credentials",
+            }),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: "Basic " + Buffer.from(clientID + ":" + secret).toString("base64"),
+            },
+        });
+        if (!response.ok) {
+            throw new Error("HTTP error! did you add API key and secret?");
+        }
+        const json = yield response.json();
+        return json.access_token;
     });
-    const json = yield response.json();
-    return json.access_token;
+    getToken().then((token) => {
+        fs_1.default.writeFileSync(".env", `Token=${token}`);
+        console.log("Token saved run artist command to get artist info");
+    });
 });
-if (argsArray.a) {
-    if (argsArray.a.length > 1) {
-        fs_1.default.writeFileSync(".env", `API=${argsArray.a[0]}\nSECRET=${argsArray.a[1]}`);
-    }
-    else {
-        console.error("Please provide both API key and secret");
-        node_process_1.default.exit(1);
-    }
-}
-if (!argv.n) {
-    console.error("Please provide a name");
-    node_process_1.default.exit(1);
-}
-else if (argv.n.length > 0) {
-    function getArtistInfo(access_token, name) {
+program
+    .command("artist")
+    .description("Get artist info")
+    .arguments("<artist>")
+    .action((artist) => {
+    function getArtistInfo() {
         return __awaiter(this, void 0, void 0, function* () {
-            const response = yield fetch(`https://api.spotify.com/v1/search?q=${name}&type=artist&limit=1`, {
+            const response = yield fetch(`https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=1`, {
                 method: "GET",
-                headers: { Authorization: "Bearer " + access_token },
+                headers: { Authorization: "Bearer " + node_process_1.default.env.Token },
             });
             if (!response.ok) {
-                throw new Error(`HTTP error! did you add API key and secret? 
+                throw new Error(`HTTP error! did you add API key and secret?
       https://developer.spotify.com/documentation/web-api/concepts/apps
       `);
             }
             return yield response.json();
         });
     }
-    getToken().then((token) => {
-        getArtistInfo(token, argv.n).then((profile) => {
-            console.log(profile.artists.items);
-        });
+    getArtistInfo().then((profile) => {
+        console.log(profile.artists.items);
     });
-}
+});
+program.parse();
--- src/index.ts
+++ src/index.ts
@@ -2,92 +2,70 @@
 
 import "dotenv/config";
 
-import yargs, { array } from "yargs";
-
-import axios from "axios";
 import fs from "fs";
 import process from "node:process";
 
-interface Arguments {
-  [x: string]: unknown;
-  _: (string | number)[];
-  $0: string;
-  n: string;
-}
+const { Command } = require("commander");
+const program = new Command();
 
-interface ArrayArgs {
-  [x: string]: unknown;
-  _: (string | number)[];
-  $0: string;
-  a: string[];
-}
+program
+  .name("string-util")
+  .description("CLI to some JavaScript string utilities")
+  .version("0.8.0");
 
-yargs
-  .usage("Usage: nodejs-spotify-cli <ts-s-cli> [options]")
-  .options({
-    n: {
-      describe: "Search with name of the artist",
-      type: "string",
-    },
-    a: {
-      describe: "Add api key and secret in [api secret] format ignore brackets",
-      type: "array",
-    },
-  })
-  .parse();
-
-const argv = yargs(process.argv).argv as Arguments;
-const argsArray = yargs.array("a").argv as ArrayArgs;
-
-const getToken = async () => {
-  const response = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    body: new URLSearchParams({
-      grant_type: "client_credentials",
-    }),
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic " +
-        Buffer.from(process.env.API + ":" + process.env.SECRET).toString(
-          "base64"
-        ),
-    },
+program
+  .command("keys")
+  .description(
+    "Input client ID and Secret from https://developer.spotify.com/dashboard/applications"
+  )
+  .arguments("<ClientID> <Secret>")
+  .action((clientID: string, secret: string) => {
+    const getToken = async () => {
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        body: new URLSearchParams({
+          grant_type: "client_credentials",
+        }),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization:
+            "Basic " + Buffer.from(clientID + ":" + secret).toString("base64"),
+        },
+      });
+      if (!response.ok) {
+        throw new Error("HTTP error! did you add API key and secret?");
+      }
+      const json = await response.json();
+      return json.access_token;
+    };
+    getToken().then((token) => {
+      fs.writeFileSync(".env", `Token=${token}`);
+      console.log("Token saved run artist command to get artist info");
+    });
   });
 
-  const json = await response.json();
-  return json.access_token;
-};
-
-if (argsArray.a) {
-  if (argsArray.a.length > 1) {
-    fs.writeFileSync(".env", `API=${argsArray.a[0]}\nSECRET=${argsArray.a[1]}`);
-  } else {
-    console.error("Please provide both API key and secret");
-    process.exit(1);
-  }
-}
-
-if (!argv.n) {
-} else if (argv.n.length > 0) {
-  async function getArtistInfo(access_token: string, name: string) {
-    const response = await fetch(
-      `https://api.spotify.com/v1/search?q=${name}&type=artist&limit=1`,
-      {
-        method: "GET",
-        headers: { Authorization: "Bearer " + access_token },
-      }
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! did you add API key and secret? 
+program
+  .command("artist")
+  .description("Get artist info")
+  .arguments("<artist>")
+  .action((artist: string) => {
+    async function getArtistInfo() {
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=1`,
+        {
+          method: "GET",
+          headers: { Authorization: "Bearer " + process.env.Token },
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! did you add API key and secret?
       https://developer.spotify.com/documentation/web-api/concepts/apps
       `);
+      }
+      return await response.json();
     }
-    return await response.json();
-  }
-  getToken().then((token) => {
-    getArtistInfo(token, argv.n).then((profile) => {
+    getArtistInfo().then((profile) => {
       console.log(profile.artists.items);
     });
   });
-}
+program.parse();
